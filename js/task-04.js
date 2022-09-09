aeab4272864e15246df1c270cd29e661e984201c
Chillbeffore <105280998+Chillbeffore@users.noreply.github.com>
2022-09-09 22:19:13 +0300

    Update task-04.js

---
+++ js/task-04.js
@@ -0,0 +1,27 @@
+const decrementBtnEl = document.querySelector(
+  '#counter button[data-action="decrement"]'
+);
+const incrementBtnEl = document.querySelector(
+  '#counter button[data-action="increment"]'
+);
+const valueEl = document.querySelector("#value");
+
+let counterValue = 0;
+
+const decrementValue = () => {
+  counterValue -= 1;
+
+  value.textContent = counterValue;
+};
+
+const incrementValue = () => {
+  counterValue += 1;
+
+  value.textContent = counterValue;
+};
+
+decrementBtnEl.addEventListener("click", decrementValue);
+incrementBtnEl.addEventListener("click", incrementValue);
+
+const counter = document.querySelector("#counter");
+console.log(counterValue);
